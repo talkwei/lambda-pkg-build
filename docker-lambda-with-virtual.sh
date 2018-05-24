cea81805a7cd18245b4981b8f84a8710f38dacf5
Vivant Shen <talkwei@gmail.com>
2018-05-24 10:56:24 +0100

    Update docker-lambda-with-virtual.sh
    update the script so as to compile with latest packages.

--- docker-lambda-with-virtual.sh
+++ docker-lambda-with-virtual.sh
@@ -2,9 +2,6 @@
 do_pip () {
 		pip install --upgrade pip wheel		
 		pip install -r /app/requirements.txt
-		# It seems scipy==0.20 is not compile with the script.
-		# scikit-learn==0.18.0 need to fix numpy_pickle_utils.py under folder of /lambda_build/lib/python3.6/site-packages/sklearn/externals/joblib/
-		# 2.0.6 has issue with my model on num_leavs.
 	}
 
 strip_virtualenv () {
@@ -13,11 +10,16 @@ strip_virtualenv () {
 		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "dataset*" | xargs rm -r
 
 		# Can't remove tests files from pandas
-		pip install pandas==0.20.3
+		pushd $VIRTUAL_ENV/lib/python3.6/site-packages/scipy/.libs && \
+		      rm *; ln ../../numpy/.libs/* . && \
+              rm -rf /root/.cache  ; popd
+
 		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.txt" | xargs rm -r
-		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.so" | xargs strip
-				
-		
+		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.so" | grep -v ufuncs | grep -v fblas | grep -v flapack | \
+                                                                    grep -v cython_blas | grep -v cython_lapack | grep -v ellip_harm | \
+                                                                    grep -v odepack | grep -v quadpack | grep -v vode | grep -v lsoda | \
+                                                                    grep -v iterative | grep -v superlu | grep -v arpack | grep -v trlib | \
+                                                                    grep -v lbfgs | grep -v qhull | xargs strip
 		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.pyc" -delete
 		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -type d -empty -delete
 		echo "current size $(du -sh $VIRTUAL_ENV | cut -f1)"
@@ -25,8 +27,10 @@ strip_virtualenv () {
 	}
 
 main () {
+        apt-get update
+        apt-get install zip unzip
 		pip install virtualenv
-		virtualenv -p /var/lang/bin/python \
+		virtualenv -p /usr/local/bin/python \
 			        --always-copy \
 			        --no-site-packages \
 			        lambda_build
